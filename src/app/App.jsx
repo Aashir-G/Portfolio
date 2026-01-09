8be9d1973ecf33c056ca4f49d970abf2b79262d9
Aashir-G <aashir.grewal@gmail.com>
2026-01-09 18:25:51 -0500

    Remove all unecessary files

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React, { useState } from 'react';
 import "./index.css";
-import Home from "./Pages/Home";
-import About from "./Pages/About";
-import AnimatedBackground from "./components/Background";
-import Navbar from "./components/Navbar";
-import Portfolio from "./Pages/Portfolio";
-import ContactPage from "./Pages/Contact";
-import ProjectDetails from "./components/ProjectDetail";
-import WelcomeScreen from "./Pages/WelcomeScreen";
+import Home from "../Pages/Home";
+import About from "../Pages/About";
+import AnimatedBackground from "../components/Background";
+import Navbar from "../components/Navbar";
+import Portfolio from "../Pages/Portfolio";
+import ContactPage from "../Pages/Contact";
+import ProjectDetails from "../components/ProjectDetail";
+import WelcomeScreen from "../Pages/WelcomeScreen";
 import { AnimatePresence } from 'framer-motion';
 
 const LandingPage = ({ showWelcome, setShowWelcome }) => {
--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,22 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
-import { collection, addDoc, getDocs } from "@firebase/firestore"; 
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { collection, addDoc } from "@firebase/firestore";
+import { getFirestore, collection, addDoc, getDocs, orderBy, query, serverTimestamp } from "firebase/firestore";
 
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCniK4loN6SjdIJV8eb3vh0YhzIs-U2_b4",
-  authDomain: "portfolio-a1201.firebaseapp.com",
-  projectId: "portfolio-a1201",
-  storageBucket: "portfolio-a1201.firebasestorage.app",
-  messagingSenderId: "52831574221",
-  appId: "1:52831574221:web:ffe40194e608adcceef184",
-  measurementId: "G-9CLK56BRF6"
-};
+    apiKey: "AIzaSyCniK4loN6SjdIJV8eb3vh0YhzIs-U2_b4",
+    authDomain: "portfolio-a1201.firebaseapp.com",
+    projectId: "portfolio-a1201",
+    storageBucket: "portfolio-a1201.firebasestorage.app",
+    messagingSenderId: "52831574221",
+    appId: "1:52831574221:web:ffe40194e608adcceef184",
+    measurementId: "G-9CLK56BRF6"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Initialize with a unique name
+const app = initializeApp(firebaseConfig, 'comments-app');
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { db, collection, addDoc };
+export { db, collection, addDoc, getDocs, orderBy, query, serverTimestamp };
